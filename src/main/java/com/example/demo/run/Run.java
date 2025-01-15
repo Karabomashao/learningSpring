1862b180eb51ac2ea47a05c7d3fe0034abd62ce8
Masego Kaudi <mkaudi023@student.wethinkcode.co.za>
2025-01-15 20:24:29 +0200

    Rest_API

--- src/main/java/com/example/demo/run/Run.java
+++ src/main/java/com/example/demo/run/Run.java
@@ -2,7 +2,7 @@ package com.example.demo.run;
 
 import java.time.LocalDateTime;
 
-public record Run(Integer Id,
+public record Run(Integer id,
                   String title,
                   LocalDateTime statedOn,
                   LocalDateTime completeOn,
